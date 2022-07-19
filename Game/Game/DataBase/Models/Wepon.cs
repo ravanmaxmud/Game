e6766e4098255341a4a5498b39547c1a96cce627
ravanmaxmud <ravanim@code.edu.az>
2022-07-19 21:43:54 +0400

    Aded Wepon Class And

--- Game/Game/DataBase/Models/Wepon.cs
+++ Game/Game/DataBase/Models/Wepon.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace Game.DataBase.Models
 {
-    public class Wepon
+    public abstract class Wepon
     {
-
+        public abstract string ShootRangeInKm();
+        public abstract string ReloaadInTime();
+    }
+    public class Ak47 : Wepon
+    {
+        public override string ShootRangeInKm()
+        {
+            return $"{2}Km";
+        }
+        public override string ReloaadInTime()
+        {
+            return $"{8}Sec";
+        }
+    }
+    public class M4 : Wepon
+    {
+        public override string ShootRangeInKm()
+        {
+            return $"{3}Km";
+        }
+        public override string ReloaadInTime()
+        {
+            return $"{8}Sec";
+        }
     }
 }
